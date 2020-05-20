4f144ef99fdcb950146ce07a2f6ec16615d7e489
Fu-Hao Lu <lufuhao@hotmail.com>
2020-05-20 16:16:02 +0800

    20200520 add gffread

---
+++ inst_gffread-latestGitHub.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="gffread"
+PackageVersTemp="version"
+InternetLink='https://github.com/gpertea/gffread.git'
+NameUncompress="gffread"
+TestCmd="./gffread --help"
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+cd ${PROGPATH}/$PackageName/$NameUncompress
+PackageVers="v"$(grep '#define VERSION ' ${PROGPATH}/$PackageName/$NameUncompress/gffread.cpp | sed 's/^.*VERSION "//;s/"$//;')"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers"
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$NameUncompress
+RunCmds "mv ${PROGPATH}/$PackageName/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/"
+cd ${PROGPATH}/$PackageName/$PackageVers/$NameUncompress
+RunCmds "make release"
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+cp LICENSE README.md gffread ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/
+$TestCmd
+if [ $? -ne 1 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/
+
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$NameUncompress
+
+exit 0
