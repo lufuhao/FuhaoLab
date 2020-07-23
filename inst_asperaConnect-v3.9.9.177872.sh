dfb8d486fae7817f95ce0031e0483d2c580f6731
Fu-Hao Lu <lufuhao@hotmail.com>
2020-07-23 17:46:41 +0800

    add alternative link

--- inst_asperaConnect-v3.9.9.177872.sh
+++ inst_asperaConnect-v3.9.9.177872.sh
@@ -10,6 +10,7 @@ source FuhaoLab.conf
 PackageName="aspera-connect"
 PackageVers="v3.9.9.177872"
 InternetLink="https://ak-delivery04-mul.dhe.ibm.com/sar/CMA/OSA/08sq3/0/ibm-aspera-connect-3.9.9.177872-linux-g2.12-64.tar.gz"
+#InternetLink="https://sourceforge.net/projects/old-software-collection/files/ibm-aspera-connect-3.9.9.177872-linux-g2.12-64.tar.gz“
 NameUncompress="ibm-aspera-connect-3.9.9.177872-linux-g2.12-64.sh"
 TestCmd="./ascp --help"
 
