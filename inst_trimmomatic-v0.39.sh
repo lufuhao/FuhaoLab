60b24c26e85d05c0c8d66a024fe11a59470ce6d9
Fu-Hao Lu <lufuhao@hotmail.com>
2020-07-27 20:41:46 +0800

    20200727 update java classpath

--- inst_trimmomatic-v0.39.sh
+++ inst_trimmomatic-v0.39.sh
@@ -7,7 +7,8 @@ PackageName="trimmomatic"
 PackageVers="v0.39"
 InternetLink="http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip"
 NameUncompress="Trimmomatic-0.39"
-TestCmd="java -jar trimmomatic-0.39.jar -version"
+#TestCmd="java -jar trimmomatic-0.39.jar -version"
+TestCmd="java org.usadellab.trimmomatic.TrimmomaticPE"
 
 
 NameCompress=$PackageName-$PackageVers.zip
@@ -22,14 +23,17 @@ if [ ! -d $PROGPATH/$PackageName/$PackageVers/x86_64 ]; then
 	fi
 fi
 cd $PROGPATH/$PackageName/$PackageVers/x86_64
-if [ -s $PROGPATH/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar ]; then
-	PrintInfo "export PATH_TRIMMOMATIC_JAR=$PROGPATH/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar"
-	export PATH_TRIMMOMATIC_JAR=$PROGPATH/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar
-	echo "### $PackageName-$PackageVers" >> $EnvironFilePath
-	echo "export PATH_TRIMMOMATIC_JAR=$PROGPATH/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar" >> $EnvironFilePath
-else
-	echo "Error: failed to install $PackageName-$PackageVers" >&2
+java -classpath ${PROGPATH}/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar org.usadellab.trimmomatic.TrimmomaticPE -version
+if [ $? -ne 0 ]; then
+	PrintInfo "Warnings: failed to run classpath $PackageName-$PackageVers"
 	exit 100
+fi
+if [ -s $PROGPATH/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar ]; then
+	AddBashrc "### $PackageName-$PackageVers"
+	AddBashrc "export PATH_TTRIMMOMATIC_ROOT=\${PROGPATH}/$PackageName/$PackageVers/x86_64"
+	AddBashrc "export PATH_TTRIMMOMATIC_ADAPTERS=\${PROGPATH}/$PackageName/$PackageVers/x86_64/adaptors"
+	AddBashrc "export PATH_TRIMMOMATIC_JAR=\${PROGPATH}/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar"
+	AddBashrc "export CLASSPATH=\${PROGPATH}/$PackageName/$PackageVers/x86_64/trimmomatic-0.39.jar:\$CLASSPATH"
 fi
 
 exit 0
