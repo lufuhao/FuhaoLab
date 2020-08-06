75192b57eb5f305d6d67fa7b052eda5a2017550b
Fu-Hao Lu <lufuhao@hotmail.com>
2020-08-06 11:22:24 +0800

    20200806 update samtools

---
+++ inst_samtools-latestGitHub.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+source FuhaoLab.conf
+#https://github.com/samtools/samtools
+
+PackageName="samtools"
+PackageVersTemp="version"
+InternetLink='https://github.com/samtools/samtools.git'
+NameUncompress="samtools"
+TestCmd="./samtools --help"
+:<<EOH
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+git clone https://github.com/samtools/htslib.git
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download HTSlib" >&2
+	exit 100
+fi
+EOH
+cd ${PROGPATH}/$PackageName/$NameUncompress
+#PackageVers="v"$(grep ^'VERSION=' version.sh | sed 's/^.*=//')
+#PrintInfo "Version: $PackageVers"
+PackageVers=$(git describe --match '[0-9].[0-9]*' --dirty --always)
+PrintInfo "Version: $PackageVers"
+
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+RunCmds "autoheader"
+RunCmds "autoconf -Wno-syntax"
+RunCmds "./configure --prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+RunCmds "make all all-htslib"
+RunCmds "make test"
+DeletePath $PROGPATH/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "make prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE install install-htslib"
+mkdir -p ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/lib ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include
+RunCmds "cp *.h ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include"
+RunCmds "cp *.a ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/lib"
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable $PROGPATH/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+DeletePath ${PROGPATH}/$PackageName/htslib
+exit 0
