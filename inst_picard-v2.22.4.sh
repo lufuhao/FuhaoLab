2bdd25426f25ec6d1a04e4a09d69a7a0ead2f767
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-03 21:45:24 +0800

    20200903 picard return 1

--- inst_picard-v2.22.4.sh
+++ inst_picard-v2.22.4.sh
@@ -13,17 +13,20 @@ CheckPath $PackageName $PackageVers
 if [ -d $PROGPATH/$PackageName/$PackageVers/$MACHTYPE ]; then
 	rm -rf $PROGPATH/$PackageName/$PackageVers/$MACHTYPE
 fi
+
 mkdir -p $PROGPATH/$PackageName/$PackageVers/$MACHTYPE/bin
 cd $PROGPATH/$PackageName/$PackageVers/$MACHTYPE/bin
 DownloadWget $InternetLink $NameCompress
+
 echo '#!/bin/bash' > picard
 echo "java -jar $PROGPATH/$PackageName/$PackageVers/$MACHTYPE/bin/picard.jar \"\$@\"" >> picard
 chmod +x picard
 $TestCmd
-if [ $? -ne 0 ]; then
+if [ $? -ne 1 ]; then
 	echo "Error: failed to install $PackageName-$PackageVers" >&2
 	exit 100
 fi
+
 cd $PROGPATH/$PackageName/$PackageVers/$MACHTYPE
 AddEnvironVariable $PROGPATH/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
 AddBashrc "export PICARD_JAR=\${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin/picard.jar"
