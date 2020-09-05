37902366c67d00b15626385420be1e706de6bd94
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-05 18:13:08 +0800

    20200905 add Unicycler

---
+++ inst_unicycler-latestGitHub.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="unicycler"
+PackageVersTemp="version"
+InternetLink='https://github.com/rrwick/Unicycler.git'
+NameUncompress="Unicycler"
+TestCmd="./unicycler-runner.py --help"
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+
+cd ${PROGPATH}/$PackageName
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers"
+RunCmds "mv ${PROGPATH}/$PackageName/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "make"
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+
+exit 0
