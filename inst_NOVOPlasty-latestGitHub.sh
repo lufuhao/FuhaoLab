7d824eeed684de0357f0bd99300107a1617a9089
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-05 17:29:04 +0800

    20200905 add NOVOPlasty

---
+++ inst_NOVOPlasty-latestGitHub.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="NOVOPlasty"
+PackageVersTemp="version"
+InternetLink='https://github.com/ndierckx/NOVOPlasty.git'
+NameUncompress="NOVOPlasty"
+TestCmd="perl ./NOVOPlasty4.2.pl --help"
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ' | sed 's/NOVOPlasty//')
+PrintInfo "Version: $PackageVers"
+
+cd ${PROGPATH}/$PackageName
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers"
+RunCmds "mv ${PROGPATH}/$PackageName/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+if [ ! -s ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/NOVOPlasty4.2.pl ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+chmod +x NOVOPlasty4.2.pl
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+AddBashrc "export NOVOPLASTY_ROOT=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+exit 0
