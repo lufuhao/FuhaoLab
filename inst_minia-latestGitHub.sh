cbc3757abd8257d1d167ffea829de42a9c18f467
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-07 15:14:26 +0800

    20200907

---
+++ inst_minia-latestGitHub.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="minia"
+PackageVersTemp="version"
+InternetLink='https://github.com/GATB/minia.git'
+NameUncompress="minia"
+TestCmd="./minia --help"
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+
+cd ${PROGPATH}/$PackageName
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers"
+RunCmds "mv ${PROGPATH}/$PackageName/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "bash ./INSTALL"
+
+#PackageVers="v3.2.4-bfd91a7"
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/build/bin
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/build
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/build "$PackageName-$PackageVers"
+
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+exit 0
