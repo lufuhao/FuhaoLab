6614b74dbbb6172ef6d772185f82d267e3950e7e
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-17 21:23:46 +0800

    20200917 add bowtie2

---
+++ inst_bowtie2-latestGitHub.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="bowtie2"
+PackageVersTemp="version"
+InternetLink='https://github.com/BenLangmead/bowtie2'
+NameUncompress="bowtie2"
+TestCmd="./bowtie2 --help"
+#PackageVers="v2.4.1-819afa7"
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --abbrev=7 --always  --long --match v* origin/master)
+#PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --always --tags --dirty)
+#PrintInfo "Version: $PackageVers"
+
+
+#cd ${PROGPATH}/$PackageName
+#DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+#RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers"
+#RunCmds "mv ${PROGPATH}/$PackageName/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+#cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+#RunCmds "make sra-deps && make USE_SRA=1"
+RunCmds "make"
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "make PREFIX=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE install"
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+exit 0
