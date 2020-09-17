abb6a81ef1597bde52baa6cbea4aba5a8903be1c
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-17 21:53:17 +0800

    20200917 add Bandage

---
+++ inst_bandage-v0.8.1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="bandage"
+PackageVers="v0.8.1"
+InternetLink="https://github.com/rrwick/Bandage/releases/download/v0.8.1/Bandage_Ubuntu_static_v0_8_1.zip"
+#NameUncompress="bowtie-1.2.3"
+TestCmd="./Bandage --help"
+
+NameCompress=$PackageName-$PackageVers.Ubuntu.static.zip
+CheckPath $PackageName $PackageVers
+DownloadWget $InternetLink $NameCompress
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+if [ ! -x ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/Bandage ]; then
+	RunCmds "unzip -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE $NameCompress"
+fi
+
+
+if [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+elif [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+else
+	PrintError "Error: install path not found: ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+	exit 100s
+fi
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+exit 0
