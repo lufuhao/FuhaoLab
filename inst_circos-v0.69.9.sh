735945c96b9d7519ab09dff958679f27e823738c
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-18 19:05:52 +0800

    20200918 add circos

---
+++ inst_circos-v0.69.9.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+source FuhaoLab.conf
+#http://circos.ca/distribution/circos-tools-0.23.tgz
+
+PackageName="circos"
+PackageVers="v0.69.9"
+InternetLink="http://circos.ca/distribution/circos-0.69-9.tgz"
+NameUncompress="circos-0.69-9"
+TestCmd="./circos -v"
+
+NameCompress=$PackageName-$PackageVers.tgz
+CheckPath $PackageName $PackageVers
+DownloadWget $InternetLink $NameCompress
+if [ ! -d $NameUncompress ]; then
+	RunCmds "tar xzvf $NameCompress"
+fi
+
+
+cd ${PROGPATH}/$PackageName/$PackageVers
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mv ${PROGPATH}/$PackageName/$PackageVers/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+#cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+#RunCmds "./configure --enable-lib --prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE --with-gmapdb=$BIODATABASES/gmapdb"
+#RunCmds "make"
+#RunCmds "make check"
+#DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+#RunCmds "make install"
+
+if [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+elif [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+else
+	PrintError "Error: install path not found: ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+	exit 100s
+fi
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddBashrc "### $PackageName-$PackageVers"
+AddBashrc "export PATH=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin:\$PATH"
+ModuleAppend "prepend-path    PATH    ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin"
+
+exit 0
