e4990f2be7799989cc373237a4e18d7ef59b0f8e
Fu-Hao Lu <lufuhao@hotmail.com>
2020-09-25 11:45:26 +0800

    20200924 make error

---
+++ NotWorking/inst_allpathslg-v52488.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+source FuhaoLab.conf
+#ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code
+#http://www.broadinstitute.org/software/allpaths?lg/blog/
+
+PackageName="allpathslg"
+PackageVers="v52488"
+InternetLink="ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/allpathslg-52488.tar.gz"
+NameUncompress="allpathslg-52488"
+TestCmd="./RunAllPathsLG --help"
+
+NameCompress=$PackageName-$PackageVers.tar.gz
+CheckPath $PackageName $PackageVers
+DownloadWget $InternetLink $NameCompress
+if [ ! -d $NameUncompress ]; then
+	RunCmds "tar xzvf $NameCompress"
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$NameUncompress
+RunCmds "./configure --prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE --enable-shared"
+RunCmds "make"
+###################################### make error
+#RunCmds "make check"
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "make install"
+
+if [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+elif [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+else
+	PrintError "Error: install path not found: ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+	exit 100s
+fi
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$NameUncompress
+exit 0
