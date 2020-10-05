34e9a68f027ee7b2f0ad2cc803f0efbe6c76c7cf
Fu-Hao Lu <lufuhao@hotmail.com>
2020-10-05 15:45:21 +0800

    20201005 failed as too many submodules

---
+++ NotWorking/inst_lib_seqan3-latestGitHub.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+source FuhaoLab.conf
+#https://github.com/seqan/seqan3
+
+PackageName="seqan3"
+PackageVersTemp="version"
+InternetLink='https://github.com/seqan/seqan3.git'
+NameUncompress="seqan3"
+
+CheckLibPath $PackageName
+
+#:<<EOM
+cd ${PROGPATH}/libraries/$PackageName/
+DeletePath ${PROGPATH}/libraries/$PackageName/$NameUncompress
+git clone --recurse-submodules $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+#EOM
+
+cd ${PROGPATH}/libraries/$PackageName/$NameUncompress
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+PackageVers=$(git describe --abbrev=7 --always  --long --match v* origin/master)
+PrintInfo "Version: $PackageVers"
+PackageVers=$(git describe --always --tags --dirty)
+PrintInfo "Version: $PackageVers"
+
+#EOM
+exit 0
+
+cd ${PROGPATH}/libraries/$PackageName/$NameUncompress
+RunCmds "autoheader"
+RunCmds "autoconf"
+RunCmds "./configure --prefix=${PROGPATH}/libraries/$PackageName/$PackageVers/$MACHTYPE"
+RunCmds "make"
+RunCmds "make test"
+DeletePath ${PROGPATH}/libraries/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "make install"
+if [ ! -d $PROGPATH/libraries/$PackageName/$PackageVers/$MACHTYPE/lib ] || [ ! -d $PROGPATH/libraries/$PackageName/$PackageVers/$MACHTYPE/include ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+#EOM
+
+cd $PROGPATH/libraries/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable $PROGPATH/libraries/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+#DeletePath ${PROGPATH}/libraries/$PackageName/$PackageVers/$NameUncompress
+exit 0
