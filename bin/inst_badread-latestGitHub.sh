9e46743e93135f5581cdb846ef0abb75d715d743
Fu-Hao Lu <lufuhao@hotmail.com>
2020-10-06 15:21:23 +0800

    20201006 add badread

---
+++ bin/inst_badread-latestGitHub.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+source FuhaoLab.conf
+#https://github.com/rrwick/Badread
+
+PackageName="badread"
+PackageVersTemp="version"
+InternetLink='https://github.com/rrwick/Badread.git'
+NameUncompress="Badread"
+TestCmd="./badread -h"
+#PackageVers="v1.10dev-9760f756"
+
+CheckPythonModules 'edlib' 'numpy' 'matplotlib' 'scipy'
+
+:<<EOM
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+EOM
+cd ${PROGPATH}/$PackageName/$NameUncompress
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+#PackageVers="v"$(grep ^'__version__' badread/version.py | sed "s/^.*\s\+=\s\+'//; s/'.*$//;")"-"$(git branch -vv | cut -f 3 -d' ')
+#PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --abbrev=7 --always  --long --match v* origin/master)
+#PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --always --tags --dirty)
+#PrintInfo "Version: $PackageVers"
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+RunCmds "python3 setup.py build"
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "python3 setup.py install --prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+FindPythonLib ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/lib
+PrintInfo "export PYTHONPATH=$PythonLibPath:\$PYTHONPATH"
+export PYTHONPATH=$PythonLibPath:$PYTHONPATH
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddBashrc "### $PackageName-$PackageVers"
+AddBashrc "export PYTHONPATH=$PythonLibPath:\$PYTHONPATH"
+AddBashrc "export PATH=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin:\$PATH"
+ModuleAppend "prepend-path    PYTHONPATH    $PythonLibPath"
+ModuleAppend "prepend-path    PATH    ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin"
+
+#DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+exit 0
