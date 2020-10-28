547c3ed89a63c77eb331e84bd0865c86e114a028
Fu-Hao Lu <lufuhao@hotmail.com>
2020-10-28 15:13:01 +0800

    20201028 delete config.h version.h from installed Include

--- bin/inst_samtools-latestGitHub.sh
+++ bin/inst_samtools-latestGitHub.sh
@@ -7,7 +7,7 @@ PackageVersTemp="version"
 InternetLink='https://github.com/samtools/samtools.git'
 NameUncompress="samtools"
 TestCmd="./samtools --help"
-:<<EOH
+
 CheckPath $PackageName
 cd ${PROGPATH}/$PackageName/
 DeletePath ${PROGPATH}/$PackageName/$NameUncompress
@@ -21,13 +21,12 @@ if [ $? -ne 0 ]; then
 	echo "Error: failed to download HTSlib" >&2
 	exit 100
 fi
-EOH
+
 cd ${PROGPATH}/$PackageName/$NameUncompress
 #PackageVers="v"$(grep ^'VERSION=' version.sh | sed 's/^.*=//')
 #PrintInfo "Version: $PackageVers"
 PackageVers=$(git describe --match '[0-9].[0-9]*' --dirty --always)
 PrintInfo "Version: $PackageVers"
-
 
 cd ${PROGPATH}/$PackageName/$NameUncompress
 RunCmds "autoheader"
@@ -39,6 +38,12 @@ DeletePath $PROGPATH/$PackageName/$PackageVers/$MACHTYPE
 RunCmds "make prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE install install-htslib"
 mkdir -p ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/lib ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include
 RunCmds "cp *.h ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include"
+if [ -s ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include/version.h ]; then
+	rm ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include/version.h
+fi
+if [ -s ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include/config.h ]; then
+	rm ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/include/config.h
+fi
 RunCmds "cp *.a ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/lib"
 
 cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
