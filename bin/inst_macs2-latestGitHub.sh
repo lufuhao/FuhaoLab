0afcfe569309d920cf684309a77774b5a430378e
Fu-Hao Lu <lufuhao@hotmail.com>
2020-11-05 15:48:19 +0800

    20201105 add MACS2 PyPI

---
+++ bin/inst_macs2-latestGitHub.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+source FuhaoLab.conf
+#https://multiqc.info/
+#https://github.com/ewels/MultiQC
+
+PackageName="macs"
+PackageVersTemp="version"
+InternetLink='https://github.com/macs3-project/MACS.git'
+NameUncompress="MACS"
+TestCmd="./macs2 --help"
+PackageVers="v2.2.7.1-2db0f0f"
+:<<EOM
+CheckPythonModules 'numpy' 'cython' 'pytest' 'pytest-cov' 'codecov' 'setuptools'
+
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+
+
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+#PackageVers="v"$(grep ^'version' setup.py | sed "s/^.*\s\+'//; s/'.*$//;")"-"$(git branch -vv | cut -f 3 -d' ')
+#PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --abbrev=7 --always  --long --match v* origin/master)
+#PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --always --tags --dirty)
+#PrintInfo "Version: $PackageVers"
+EOM
+cd ${PROGPATH}/$PackageName/$NameUncompress
+RunCmds "python3 setup.py build"
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "python3 setup.py install --prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+FindPythonLib ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/lib
+PrintInfo "export PYTHONPATH=$PythonLibPath:\$PYTHONPATH"
+export PYTHONPATH=$PythonLibPath:$PYTHONPATH
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddBashrc "### $PackageName-$PackageVers"
+AddBashrc "export PYTHONPATH=$PythonLibPath:\$PYTHONPATH"
+AddBashrc "export PATH=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin:\$PATH"
+ModuleAppend "prepend-path    PYTHONPATH    $PythonLibPath"
+ModuleAppend "prepend-path    PATH    ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin"
+
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+
+exit 0
