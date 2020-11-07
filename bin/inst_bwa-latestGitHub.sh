d953a8e9c64ae7e675b88eeba2c79492d0cc9486
Fu-Hao Lu <lufuhao@hotmail.com>
2020-11-07 12:09:55 +0800

    20201107 add BWA

---
+++ bin/inst_bwa-latestGitHub.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="bwa"
+PackageVersTemp="version"
+InternetLink='https://github.com/lh3/bwa.git'
+NameUncompress="bwa"
+TestCmd="./bwa"
+#PackageVers="v0.7.17-13b5637"
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+#AC_INIT([MUMmer], [4.0.0beta2], [gmarcais@umd.edu])
+#PackageVers="v"$(grep 'AC_INIT' ${PROGPATH}/$PackageName/$NameUncompress/configure.ac | sed 's/^AC_INIT.*jellyfish\], \[//;s/\].*$//g')
+#PrintInfo "Version: $PackageVers"
+PackageVers=$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --abbrev=7 --always  --long --match v* origin/master)
+#PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --always --tags --dirty)
+#PrintInfo "Version: $PackageVers"
+
+
+cd ${PROGPATH}/$PackageName
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers"
+RunCmds "mv ${PROGPATH}/$PackageName/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+#cd ${PROGPATH}/$PackageName/$NameUncompress
+RunCmds "make"
+RunCmds "mkdir -p bin include lib man/man1"
+RunCmds "mv bwa qualfa2fq.pl xa2multi.pl bin/"
+RunCmds "mv *.h include/"
+RunCmds "mv libbwa.a lib/"
+RunCmds "mv bwa.1 man/man1/"
+#RunCmds "rm *.md Makefile COPYING ChangeLog bwa-helper.js"
+
+if [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+elif [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+else
+	PrintError "Error: install path not found: ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+	exit 100
+fi
+$TestCmd
+if [ $? -ne 1 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+#DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+exit 0
