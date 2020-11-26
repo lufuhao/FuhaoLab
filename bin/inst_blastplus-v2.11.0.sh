96cadac6372d049408894a7b40be908e3d6ad33a
Fu-Hao Lu <lufuhao@hotmail.com>
2020-11-26 16:01:30 +0800

    20201126 Update blast to v2.11.0

---
+++ bin/inst_blastplus-v2.11.0.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source FuhaoLab.conf
+### https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/
+
+PackageName="blastplus"
+PackageVers="v2.11.0"
+InternetLink="https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.11.0+-x64-linux.tar.gz"
+NameUncompress="ncbi-blast-2.11.0+"
+TestCmd="./blastn -help"
+MACHTYPE="x64"
+
+NameCompress=$PackageName-$PackageVers.x64.linux.tar.gz
+CheckPath $PackageName $PackageVers
+DownloadWget $InternetLink $NameCompress
+if [ ! -d $NameUncompress ]; then
+	RunCmds "tar xzvf $NameCompress"
+fi
+
+cd $PROGPATH/$PackageName/$PackageVers/$NameUncompress
+RunCmds "mv $PROGPATH/$PackageName/$PackageVers/$NameUncompress $PROGPATH/$PackageName/$PackageVers/$MACHTYPE"
+cd $PROGPATH/$PackageName/$PackageVers/$MACHTYPE/bin
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd $PROGPATH/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable $PROGPATH/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+exit 0
