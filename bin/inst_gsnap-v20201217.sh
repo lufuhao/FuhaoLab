e401484859f839b5d8188cf2ec77fdc376189e9a
Fu-Hao Lu <lufuhao@hotmail.com>
2020-12-30 18:53:31 +0800

    20201230 gsnap 20201217

---
+++ bin/inst_gsnap-v20201217.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+### HOMEPAGE: http://research-pub.gene.com/gmap/
+### Require: zlib bzip2
+source FuhaoLab.conf
+
+
+PackageName="gsnap"
+PackageVers="v20201217"
+InternetLink="http://research-pub.gene.com/gmap/src/gmap-gsnap-2020-12-17.tar.gz"
+NameUncompress="gmap-2020-12-17"
+
+
+
+if [ -z "$BIODATABASES" ]; then
+	PrintError: "Error: GMAPDB would be NOT set as $BIODATABASES not defined"
+	exit 100
+fi
+
+NameCompress=$PackageName-$PackageVers.tar.gz
+CheckPath $PackageName $PackageVers
+DeletePath $PROGPATH/$PackageName/$PackageVers/$NameUncompress
+DownloadWget $InternetLink $NameCompress
+if [ ! -d $NameUncompress ]; then
+	RunCmds "tar xzvf $NameCompress"
+fi
+
+cd $PROGPATH/$PackageName/$PackageVers/$NameUncompress
+if [ -z "$GMAPDB" ]; then
+	GmapDatabase=$BIODATABASES/gmapdb
+else
+	GmapDatabase=$GMAPDB
+fi
+if [ ! -d $GmapDatabase ]; then
+	mkdir -p $GmapDatabase
+fi
+RunCmds "./configure --enable-lib --prefix=$PROGPATH/$PackageName/$PackageVers/x86_64 --with-gmapdb=$BIODATABASES/gmapdb --enable-zlib --enable-bzlib"
+RunCmds "make"
+RunCmds "make check"
+RunCmds "make install"
+cd $PROGPATH/$PackageName/$PackageVers/x86_64/bin
+./gsnap --help
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+cd $PROGPATH/$PackageName/$PackageVers/x86_64
+AddEnvironVariable $PROGPATH/$PackageName/$PackageVers/x86_64 "$PackageName-$PackageVers"
+if [ -z "$GMAPDB" ]; then
+	PrintInfo "Info: GMAPDB was set to $GmapDatabase"
+	AddBashrc "export GMAPDB=$GmapDatabase"
+	ModuleAppend "setenv    GMAPDB    $GmapDatabase"
+fi
+
+DeletePath $PROGPATH/$PackageName/$PackageVers/$NameUncompress
+exit 0
