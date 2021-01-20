067f3d853190590c5d487434dda6f1a783e45672
Fu-Hao Lu <lufuhao@hotmail.com>
2021-01-20 17:25:22 +0800

    update GATK to v4.1.9.0

---
+++ bin/inst_gatk-v4.1.9.0.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="gatk"
+PackageVers="v4.1.9.0"
+InternetLink="https://github.com/broadinstitute/gatk/releases/download/4.1.9.0/gatk-4.1.9.0.zip"
+NameUncompress="gatk-4.1.9.0"
+TestCmd="./gatk --list"
+
+CmdExists "java"
+
+NameCompress=$PackageName-$PackageVers.zip
+CheckPath $PackageName $PackageVers
+DownloadWget $InternetLink $NameCompress
+if [ ! -d $NameUncompress ]; then
+	RunCmds "unzip $NameCompress"
+fi
+
+
+
+cd ${PROGPATH}/$PackageName/$PackageVers
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mv ${PROGPATH}/$PackageName/$PackageVers/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+
+if [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
+elif [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE ]; then
+	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+else
+	PrintError "Error: install path not found: ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+	exit 100s
+fi
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+
+exit 0
