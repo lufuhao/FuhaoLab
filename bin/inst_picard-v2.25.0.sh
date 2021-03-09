62584f47f27abb235dbfd6bd28c6f2dd72c804dc
Fu-Hao Lu <lufuhao@hotmail.com>
2021-03-09 11:29:38 +0800

    20210309 update picard to 2.25.0

---
+++ bin/inst_picard-v2.25.0.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="picard"
+PackageVers="v2.25.0"
+InternetLink="https://github.com/broadinstitute/picard/releases/download/2.25.0/picard.jar"
+TestCmd="./picard"
+
+
+NameCompress="picard.jar"
+CheckPath $PackageName $PackageVers
+DeletePath $PROGPATH/$PackageName/$PackageVers/$MACHTYPE
+
+mkdir -p $PROGPATH/$PackageName/$PackageVers/$MACHTYPE/bin
+cd $PROGPATH/$PackageName/$PackageVers/$MACHTYPE/bin
+DownloadWget $InternetLink $NameCompress
+
+echo '#!/bin/bash' > picard
+echo "java -jar $PROGPATH/$PackageName/$PackageVers/$MACHTYPE/bin/picard.jar \"\$@\"" >> picard
+chmod +x picard
+$TestCmd
+if [ $? -ne 1 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd $PROGPATH/$PackageName/$PackageVers/$MACHTYPE
+AddEnvironVariable $PROGPATH/$PackageName/$PackageVers/$MACHTYPE "$PackageName-$PackageVers"
+AddBashrc "export PICARD_JAR=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin/picard.jar"
+ModuleAppend "setenv    PICARD_JAR    ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin/picard.jar"
+
+exit 0
