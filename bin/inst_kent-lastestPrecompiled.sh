7116cb2b0c503c37f08576a61068614865b42142
Fu-Hao Lu <lufuhao@hotmail.com>
2021-07-16 18:37:03 +0800

    20210716 add version

--- bin/inst_kent-lastestPrecompiled.sh
+++ bin/inst_kent-lastestPrecompiled.sh
@@ -3,18 +3,24 @@ source FuhaoLab.conf
 #http://hgdownload.cse.ucsc.edu/admin/
 
 PackageName="kent"
-PackageVers="vlatest"
+PackageVersTmp="vlatest"
 #InternetLink="http://hgdownload.cse.ucsc.edu/admin/jksrc.v403.zip"
 #NameUncompress="kent"
 #TestCmd="./bowtie --help"
-MACHTYPE="x64"
+MACHTYPE="x86_64"
 
-CheckPath $PackageName $PackageVers
-DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
-RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
-cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+PrintError "Warnings: It would take long time, and be patient..."
+
+CheckPath $PackageName $PackageVersTmp
+DeletePath ${PROGPATH}/$PackageName/$PackageVersTmp/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVersTmp/$MACHTYPE"
+cd ${PROGPATH}/$PackageName/$PackageVersTmp/$MACHTYPE
 RunCmds "rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/ ./"
 #RunCmds "rsync -aP rsync://hgdownload-sd.soe.ucsc.edu/genome/admin/exe/linux.x86_64/ ./"
+
+cd ${PROGPATH}/$PackageName
+PackageVers="v"$(stat -c '%y%m' twoBitToFa | sed 's/\s\+.*$//;s/-//g;')
+mv ${PROGPATH}/$PackageName/$PackageVersTmp ${PROGPATH}/$PackageName/$PackageVers
 
 if [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin ]; then
 	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/bin
@@ -22,7 +28,7 @@ elif [ -d ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE ]; then
 	cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
 else
 	PrintError "Error: install path not found: ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
-	exit 100s
+	exit 100
 fi
 $TestCmd
 if [ $? -ne 0 ]; then
