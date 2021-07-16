78d5030e8740b5bc14c7ee9dd880f4e6e4c2fa33
Fu-Hao Lu <lufuhao@hotmail.com>
2021-07-16 11:56:08 +0800

    20210716 add NINJA

---
+++ bin/inst_ninja-latestGitHub-cluster_only.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+source FuhaoLab.conf
+
+
+PackageName="ninja"
+PackageVersTemp="version"
+InternetLink='https://github.com/TravisWheelerLab/NINJA.git'
+NameUncompress="NINJA"
+TestCmd="./Ninja --help"
+#PackageVers=""
+
+CheckPath $PackageName
+cd ${PROGPATH}/$PackageName/
+DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+git clone $InternetLink
+if [ $? -ne 0 ]; then
+	echo "Error: failed to download $PackageName" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$NameUncompress
+#AC_INIT([MUMmer], [4.0.0beta2], [gmarcais@umd.edu])
+#PackageVers="v"$(grep 'AC_INIT' ${PROGPATH}/$PackageName/$NameUncompress/configure.ac | sed 's/^AC_INIT.*jellyfish\], \[//;s/\].*$//g')
+#PrintInfo "Version: $PackageVers"
+PackageVers="v"$(git tag -l | tail -n 1)"-"$(git branch -vv | cut -f 3 -d' ')
+PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --abbrev=7 --always  --long --match v* origin/master)
+#PrintInfo "Version: $PackageVers"
+#PackageVers=$(git describe --always --tags --dirty)
+#PrintInfo "Version: $PackageVers"
+
+
+cd ${PROGPATH}/$PackageName
+DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+RunCmds "mkdir -p ${PROGPATH}/$PackageName/$PackageVers"
+RunCmds "mv ${PROGPATH}/$PackageName/$NameUncompress ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+#cd ${PROGPATH}/$PackageName/$NameUncompress
+#RunCmds "cmake -DCMAKE_INSTALL_PREFIX=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE .."
+#RunCmds "./configure --prefix=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE"
+RunCmds "make build"
+RunCmds "make docs"
+RunCmds "make check"
+#DeletePath ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+#RunCmds "make install"
+
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/NINJA
+$TestCmd
+if [ $? -ne 0 ]; then
+	echo "Error: failed to install $PackageName-$PackageVers" >&2
+	exit 100
+fi
+
+cd ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE
+AddBashrc "### $PackageName-$PackageVers"
+AddBashrc "export PATH=${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/NINJA:\$PATH"
+ModuleAppend "prepend-path    PATH    ${PROGPATH}/$PackageName/$PackageVers/$MACHTYPE/NINJA"
+
+#DeletePath ${PROGPATH}/$PackageName/$NameUncompress
+exit 0
